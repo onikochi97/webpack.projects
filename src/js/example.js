eb6520a2aa83c0756d7323858f6283eda04b4df9
onikochi97 <dj_rus_2000@mail.ru>
2021-04-14 20:42:48 +0400

    first commit

--- src/js/example.js
+++ src/js/example.js
@@ -1,4 +1,4 @@
-export const example = () => {
+export default () => {
   return (
     'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
     'with a hot dev server and an optimized production build.'
